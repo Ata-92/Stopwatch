f885caa83b7b3b0870907b0721dd7abc6596980a
Ata-92 <atakal92@gmail.com>
2021-05-26 06:37:10 +0300

    Select elements from html

---
+++ app.js
@@ -0,0 +1,6 @@
+const playPauseBtn = document.querySelector(".play-pause");
+const stopBtn = document.querySelector(".stop");
+const icons = document.querySelectorAll("i");
+const minute = document.querySelector(".minute");
+const second = document.querySelector(".second");
+const milisecond10 = document.querySelector(".milisecond-10");
