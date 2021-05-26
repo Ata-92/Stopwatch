09d925a74ea6b065b0f9f3ee9fe820b689ad809b
Ata-92 <atakal92@gmail.com>
2021-05-26 06:38:42 +0300

    Define other variables

--- app.js
+++ app.js
@@ -4,3 +4,12 @@ const icons = document.querySelectorAll("i");
 const minute = document.querySelector(".minute");
 const second = document.querySelector(".second");
 const milisecond10 = document.querySelector(".milisecond-10");
+
+let converter = 1;
+icons[converter].style.display = "none";
+
+minute.innerHTML = "00";
+second.innerHTML = "00";
+milisecond10.innerHTML = "00";
+
+let play = false;
